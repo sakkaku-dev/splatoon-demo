867ef1588368e11cdc13067f2673095cd4adacfb
myin <minyin142p@gmail.com>
2024-10-12 20:44:00 +0200

    keep old code

--- paint/PaintBall.gd
+++ paint/PaintBall.gd
@@ -20,7 +20,21 @@ func _physics_process(delta: float) -> void:
 	velocity.y -= gravity * delta
 	look_at(position + velocity)
 	
-	move_and_slide()
+	move_and_slide()	
+	
+	# Previous version used a camera, might still be needed in the future
+	#if not body.has_method("get_textures"):
+		#queue_free()
+		#return
+	#
+	#var texture: Textures = body.get_textures()
+	#var orth = direction.rotated(Vector3.RIGHT, deg_to_rad(90))
+	#spring_arm.look_at(direction, orth)
+	
+	#var pos = camera.unproject_position(global_transform.origin) / get_viewport().size
+	#var paint_layer = texture.get_paint_layer(Textures.Slot.ALBEDO)
+	#paint_layer.do_paint(pos, 4, camera, Color(1.0, 1.0, 1.0, 1.0), body.global_transform.origin)
+	#queue_free()
 
 	if get_slide_collision_count() > 0:
 		for i in range(0, get_slide_collision_count()):
